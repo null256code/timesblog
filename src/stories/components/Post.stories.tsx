1a71e0344990da6c9183bd3987ae108b0777e6a4
null256code <19668528+null256code@users.noreply.github.com>
2024-02-17 16:58:20 +0900

    feat(storybook): Post.storiesを作成

---
+++ src/stories/components/Post.stories.tsx
@@ -0,0 +1,88 @@
+import Post from "@/components/Post";
+import { Meta, StoryObj } from "@storybook/react";
+
+const sampleBody = (
+  <>
+    <p>ブログを作った。</p>
+    <p>
+      去年からNext.jsの勉強がてら作ってみるかと、チュートリアルをやりながらズルズル作ったり放置したりして今完成した。
+      <br />
+      一応コンセプトがあり、Slackのtimesチャンネルのような極力記事の投稿するハードルが下がるようなメモ書きのようなブログにしたいと思っている。
+    </p>
+    <p>
+      普段の自分のtimesチャンネルから、残した方が良さそうだなというものをこっちに転記するというような使い方をする、かも。
+    </p>
+  </>
+);
+
+interface MicroCMSDate {
+  createdAt: string;
+  updatedAt: string;
+  publishedAt?: string;
+  revisedAt?: string;
+}
+
+const mockDate = {
+  createdAt: "2024-01-01",
+  updatedAt: "2024-01-01",
+} satisfies MicroCMSDate;
+
+const sampleTags = [
+  {
+    tagKey: "sample1",
+    tagName: "サンプル1",
+    isVisibleInMenu: true,
+    ...mockDate,
+  },
+  {
+    tagKey: "sample2",
+    tagName: "サンプル2",
+    isVisibleInMenu: false,
+    ...mockDate,
+  },
+];
+
+const meta = {
+  title: "Post",
+  component: Post,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  argTypes: {},
+} satisfies Meta<typeof Post>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Normal: Story = {
+  args: {
+    children: sampleBody,
+    title: "ブログを作った",
+    postedTime: new Date(),
+    tags: sampleTags,
+    contentId: "TEST",
+  },
+};
+
+export const NoTitle: Story = {
+  args: {
+    children: sampleBody,
+    postedTime: new Date(),
+    tags: sampleTags,
+    contentId: "TEST",
+  },
+};
+
+export const ChildPost: Story = {
+  args: {
+    children: sampleBody,
+    postedTime: new Date(),
+    tags: sampleTags,
+    contentId: "TEST",
+    rootPost: {
+      contentId: "TEST",
+      title: "親投稿タイトル",
+    },
+  },
+};
